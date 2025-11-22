eaab1e7c9b5155ec42721f486598383690425d55
Gading Satrio <gadingsatrio546@gmail.com>
2025-11-22 19:56:11 +0700

    Fix: add suppressHydrationWarning to html tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="id" className="scroll-smooth">
+    <html lang="id" className="scroll-smooth" suppressHydrationWarning>
       <body
         className={`${plusJakartaSans.variable} font-sans antialiased`}
       >
